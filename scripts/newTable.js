ef9cfef8b8715ebf85acf808d962bb229c5827a1
Pitt05 <pierre.bouffier05@gmail.com>
2016-10-07 09:58:04 +0200

    Upgrade newTable.js !

--- scripts/newTable.js
+++ scripts/newTable.js
@@ -7,6 +7,7 @@ class Table {
         this.y = Math.floor((Math.random() * 400) + 100);
         this.svg = svg;
         this.group = null;
+        this.rect = null;
         this.selected = false;
         this.displayed = false;
 
@@ -20,6 +21,7 @@ class Table {
         var newGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
         newGroup.setAttribute("id", "group_" + this.name);
         newGroup.setAttribute("class", "table_group");
+        newGroup.setAttribute("transform", 'translate(' + this.x + ' ' + this.y + ')');
         this.group = newGroup;
         this.svg.appendChild(newGroup);
     }
@@ -29,16 +31,18 @@ class Table {
         // Création du rectangle
         var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
         rect.setAttribute("class", "table_rect");
-        rect.setAttribute("x", this.x);
-        rect.setAttribute("y", this.y);
         rect.setAttribute("rx", 6);
         rect.setAttribute("ry", 6);
+        rect.setAttribute("height", 40);
+        this.rect = rect;
         this.group.appendChild(rect);
 
         // Création du nom de table_group
         var textName = document.createElementNS("http://www.w3.org/2000/svg", "text");
         textName.setAttribute("class", "table_name name_" + this.name);
-        textName.setAttribute("transform", 'translate(' + (this.x + 10) + ' ' + (this.y + 28) + ')');
+        textName.setAttribute("id", "ZORRO");
+        textName.setAttribute("x", 10);
+        textName.setAttribute("y", 28);
         textName.textContent = this.name;
         this.group.appendChild(textName);
 
@@ -48,6 +52,17 @@ class Table {
     restructureTable()
     {
         // TODO: Déterminé le plus grand texte dans le rectangle afin de le dimensionner
+        // INFO: Les marges à gauche sont TOUJOURS de 10px
+        var maxWidth = 0;
+        var positionInfo;
+        // Calcul largeur textName
+        var textName = document.getElementsByClassName("name_" + this.name)[0];
+        positionInfo = textName.getBoundingClientRect().width;
+        maxWidth = 20 + positionInfo;
+
+
+        // Set the MaxWidth to width's rect attribute
+        this.rect.setAttribute("width", maxWidth);
     }
 
 }
